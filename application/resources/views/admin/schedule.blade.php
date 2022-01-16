6e2e48d55aec0310ea64de11c99aabd7655bd942
Andrei Lazaro <andr3ilazaro@gmail.com>
2022-01-17 00:44:40 +0800

    fixed System Setup - Schedules blade

--- application/resources/views/admin/schedule.blade.php
+++ application/resources/views/admin/schedule.blade.php
@@ -22,7 +22,7 @@
                                 <path d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z" fill="black" />
                             </svg>
                         </span>
-                        <input type="text" data-kt-course-table-filter="search" class="form-control form-control-solid w-250px ps-15" placeholder="Search Course" />
+                        <input type="text" data-kt-schedule-table-filter="search" class="form-control form-control-solid w-250px ps-15" placeholder="Search Schedule" />
                     </div>
                 </div>
                 <div class="card-toolbar">
@@ -38,7 +38,7 @@
                             </span>
                             Export
                         </button>
-                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#kt_modal_add_schedule">Add Course</button>
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#kt_modal_add_schedule">Add Schedule</button>
                     </div>
                     <div class="d-flex justify-content-end align-items-center d-none" data-kt-customer-table-toolbar="selected">
                         <div class="fw-bolder me-5">
@@ -179,10 +179,7 @@
                 <input type="text" name="id" hidden />
 
                 <div class="modal-header" id="kt_modal_edit_schedule_header">
-                    <!--begin::Modal title-->
-                    <h2 class="fw-bolder">Edit Course</h2>
-                    <!--end::Modal title-->
-                    <!--begin::Close-->
+                    <h2 class="fw-bolder">Edit Schedule</h2>
                     <div id="kt_modal_edit_schedule_close" class="btn btn-icon btn-sm btn-active-icon-primary">
                         <!--begin::Svg Icon | path: icons/duotune/arrows/arr061.svg-->
                         <span class="svg-icon svg-icon-1">
@@ -193,7 +190,6 @@
                         </span>
                         <!--end::Svg Icon-->
                     </div>
-                    <!--end::Close-->
                 </div>
 
                 <div class="modal-body py-10 px-lg-17">
@@ -494,7 +490,7 @@
             ],
         });
 
-        $('[data-kt-course-table-filter="search"]').on('keyup', function(e) { // Search bar 
+        $('[data-kt-schedule-table-filter="search"]').on('keyup', function(e) { // Search bar 
             table.search(e.target.value).draw();
         });
 
