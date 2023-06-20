51363acb917e011440b8fc7d4c3168c66fcb9a92
J-Cayetano <cayetanojohnchris@gmail.com>
2023-06-20 14:18:51 +0800

    Student Scholastic Record (PDF)
    Generation of Scholastic Record PDF per Students

--- app/Http/Controllers/Admin/StudentProfileController.php
+++ app/Http/Controllers/Admin/StudentProfileController.php
@@ -414,17 +414,170 @@ class StudentProfileController extends Controller
     {
         abort_if($student->stud_recordType != 'NONSIS', Response::HTTP_NOT_FOUND);
 
-        $pdf = Pdf::loadView('admin.student_profile.pdf.scholastic-data', compact('student'));
+        $tp = $student->leftJoin('s_course', 'cour_stud_id', '=', 'cour_id')
+            ->selectRaw('r_student.*
+            , md5(stud_id) as stud_id_md5
+            , cour_name as stud_course_name
+            , cour_code as stud_course 
+            , DATE_FORMAT(stud_createdAt, "%Y-%m-%d") as `stud_createdAt_m_f`
+            , DATE_FORMAT(stud_createdAt, "%r") as `stud_createdAt_t_f`
+            , IFNULL(DATE_FORMAT(stud_updatedAt, "%Y-%m-%d"), "") as `stud_updatedAt_m_f`
+            , IFNULL(DATE_FORMAT(stud_updatedAt, "%r"), "") as `stud_updatedAt_t_f`
+            ')
+            ->where('stud_id', $student->stud_id)
+            ->get();
 
-        $pdf->render();
-        $canvas = $pdf->getCanvas();
 
-        $height = $canvas->get_height();
+        if (sizeof($tp) == 1) {
 
-        $canvas->set_opacity(.5, "Multiply");
-        $canvas->page_text(35, $height - 30, "Page {PAGE_NUM} of {PAGE_COUNT}", null, 10, array(0, 0, 0));
+            $vd = [];
 
-        return $pdf->stream();
+            $fp = $tp[0];
+            $sId = $fp['stud_id'];
+
+            $ps = new PreviousSchool();
+            $fp['stud_sPrimary'] = $ps->where(["extsch_stud_id" => $sId, "extsch_educType" => "PRIMARY"])
+                ->get();
+
+            $fp['stud_sSecondary'] = $ps->where(["extsch_stud_id" => $sId, "extsch_educType" => "SECONDARY"])
+                ->get();
+
+            $fp['stud_sPrimary'] = (sizeOf($fp['stud_sPrimary']) == 1) ? $fp['stud_sPrimary'][0] : [];
+            $fp['stud_sSecondary'] = (sizeOf($fp['stud_sSecondary']) == 1) ? $fp['stud_sSecondary'][0] : [];
+
+
+            if ($fp['stud_admissionType'] == "TRANSFEREE" || $fp['stud_admissionType'] == "LADDERIZED") {
+
+                $fp['stud_sTertiary'] = $ps->where(["extsch_stud_id" => $sId, "extsch_educType" => "TRANSFER"])
+                    ->get();
+            }
+
+            if ($fp['stud_recordType'] == "NONSIS") {
+                $g = [];
+                $g['total_semester'] = 0;
+                $g['total_summer_semester'] = 0;
+
+                $sg = new StudentGrades();
+                $school_year = $sg->leftJoin('s_class', 'class_enrsub_id', '=', 'class_id')
+                    ->selectRaw('DISTINCT class_acadYear as acad_year
+                        , CONCAT(class_acadYear, " - ", class_acadYear + 1) as acad_year_long
+                        , CONCAT(class_acadYear, "-\'" ,SUBSTRING(class_acadYear + 1, 3, 2)) as acad_year_short
+                        ')
+                    ->whereRaw('stud_enrsub_id = "' . $sId . '"')
+                    ->orderBy('acad_year', 'desc')
+                    ->get();
+
+                $i = 0;
+                foreach ($school_year as $year) {
+
+                    $semesters = $sg->leftJoin('s_class', 'class_enrsub_id', '=', 'class_id')
+                        ->leftJoin('s_term', 'class_term_id', '=', 'term_id')
+                        ->selectRaw('DISTINCT term_name, term_order')
+                        ->whereRaw('stud_enrsub_id = "' . $sId . '" and class_acadYear = "' . $year->acad_year . '"')
+                        ->orderBy('term_order', 'desc')
+                        ->get();
+
+                    $a = 0;
+                    foreach ($semesters as $semester) {
+                        $g['total_semester'] += 1;
+
+                        if ($semester['term_name'] == "Summer Semester") {
+                            $g['total_summer_semester'] += 1;
+                        }
+
+                        $grades = $sg->leftJoin('r_student', 'stud_enrsub_id', '=', 'stud_id')
+                            ->leftJoin('s_class', 'class_enrsub_id', '=', 'class_id')
+                            ->leftJoin('s_instructor', 'class_inst_id', '=', 'inst_id')
+                            ->leftJoin('s_subject', 'class_subj_id', '=', 'subj_id')
+                            ->leftJoin('s_term', 'class_term_id', '=', 'term_id')
+                            ->selectRaw('t_student_enrolled_subjects.*
+                    , md5(enrsub_id) as enrsub_id_md5
+                    , inst_firstName as enrsub_inst_firstName
+                    , inst_middleName as enrsub_inst_middleName
+                    , inst_lastName as enrsub_inst_lastName
+                    , inst_suffix as enrsub_inst_suffixName
+                    , subj_code as enrsub_subj_code
+                    , subj_name as enrsub_subj_name
+                    , subj_units as enrsub_subj_units
+                    , class_section as enrsub_sche_section
+                    ')
+                            ->whereRaw('stud_enrsub_id = "' . $sId . '" and term_name = "' . $semester->term_name . '"and class_acadYear = "' . $year->acad_year . '"')
+                            ->get();
+
+                        $b = 0;
+                        foreach ($grades as $grade) {
+
+
+                            $grades[$b]['enrsub_grade_display'] = $grade['enrsub_finalRating'];
+                            $grades[$b]['enrsub_grade_display_status'] = $grade['enrsub_grade_status'];
+
+                            $grades[$b]['enrsub_inst_fullName'] = format_name(1, "",  $grades[$b]['enrsub_inst_firstName'], $grades[$b]['enrsub_inst_middleName'], $grades[$b]['enrsub_inst_lastName'], $grades[$b]['enrsub_inst_suffixName']);
+
+                            $b++;
+                        }
+
+                        $semesters[$a]['subjects'] = $grades;
+                        $a++;
+                    }
+
+                    $school_year[$i]['semesters'] = $semesters;
+                    $i++;
+                }
+
+                $g["grades"] = $school_year;
+
+                $vd = array_merge($vd, [
+                    "stud_grades" => $g['grades'], "stud_total_semester" => $g['total_semester'], "stud_total_summer_semester" => $g['total_summer_semester']
+                ]);
+            }
+
+            $dsi = new DocumentsSubmitted();
+            $ds["entrance"] = $dsi->leftjoin("s_documents", 'subm_document', '=', 'docu_id')
+                ->where(["subm_documentCategory" => "ENTRANCE", "subm_student" => $sId])->get();
+            $ds["records"] = $dsi->leftjoin("s_documents", 'subm_document', '=', 'docu_id')
+                ->where(["subm_documentCategory" => "RECORDS", "subm_student" => $sId, "docu_isPermanent" => "NO"])->get();
+            $ds["exit"] = $dsi->leftjoin("s_documents", 'subm_document', '=', 'docu_id')
+                ->where(["subm_documentCategory" => "EXIT", "subm_student" => $sId])->get();
+
+
+            // Get fixed Documents
+
+            // -- Records
+
+            // --- Registration Cards
+            $dfs["records"]['regcert'] = $dsi->leftjoin("s_documents", 'subm_document', '=', 'docu_id')
+                ->where(["subm_documentCategory" => "RECORDS", "subm_student" => $sId, "docu_id" => "1"])
+                ->orderBy('subm_documentType', 'desc')
+                ->orderBy('subm_documentType_1', 'desc')->get();
+
+            // return $vd['stud_grades'][0];
+            $pdf = Pdf::loadView('admin.student_profile.pdf.scholastic-data', compact('student', 'vd'));
+
+            $pdf->render();
+            $canvas = $pdf->getCanvas();
+
+            $height = $canvas->get_height();
+
+            $canvas->set_opacity(.5, "Multiply");
+            $canvas->page_text(35, $height - 30, "Page {PAGE_NUM} of {PAGE_COUNT}", null, 10, array(0, 0, 0));
+
+            return $pdf->stream();
+        } else {
+
+            return view('errors.not-found', ['menu_header' => 'Menu', 'title' => "Profile not found", "menu" => "student-records", "sub_menu" => "student-profile"]);
+        };
+
+        // $pdf = Pdf::loadView('admin.student_profile.pdf.scholastic-data', compact('student'));
+
+        // $pdf->render();
+        // $canvas = $pdf->getCanvas();
+
+        // $height = $canvas->get_height();
+
+        // $canvas->set_opacity(.5, "Multiply");
+        // $canvas->page_text(35, $height - 30, "Page {PAGE_NUM} of {PAGE_COUNT}", null, 10, array(0, 0, 0));
+
+        // return $pdf->stream();
     }
 
     public function generateEnvelopeTag(StudentProfile $student)
--- resources/views/admin/student_profile/pdf/scholastic-data.blade.php
+++ resources/views/admin/student_profile/pdf/scholastic-data.blade.php
@@ -68,127 +68,60 @@
     <hr />
 
     <div>
-        <table class="table table-bordered table-sm">
-            <thead>
-                <tr class="table-active bg-secondary">
-                    <th scope="col" colspan="6" class="py-3"><span class="text-uppercase">SY 2000 - 2001</span>
-                    </th>
-                </tr>
-                <tr class="table-active">
-                    <th scope="col"
-                        width="5%"class="text-center font-weight-normal small align-middle font-weight-bold">NO.
-                    </th>
-                    <th scope="col" width="15%" class="font-weight-normal small align-middle font-weight-bold">
-                        SUBJECT CODE</th>
-                    <th scope="col" width="65%" class="font-weight-normal small align-middle font-weight-bold">
-                        DESCRIPTION</th>
-                    <th scope="col" width="5%" class="font-weight-normal small align-middle font-weight-bold">
-                        UNITS</th>
-                    <th scope="col" width="5%" class="font-weight-normal small align-middle font-weight-bold">
-                        FINAL GRADE</th>
-                    <th scope="col" width="5%" class="font-weight-normal small align-middle font-weight-bold">
-                        GRADE STATUS</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="col" colspan="6"class="align-middle text-center"><span class="text-uppercase">2nd
-                            Semester</span>
-                    </th>
-                </tr>
-                <tr>
-                    <th class="text-center">1.</th>
-                    <td class="text-center">MT 123</td>
-                    <td>COLLEGE ALGEBRA</td>
-                    <td class="text-center">3.0</td>
-                    <td class="text-center">2.50</td>
-                    <td class="text-center">P</td>
-                </tr>
-                <tr>
-                    <th scope="col" colspan="6"class="align-middle text-center"><span class="text-uppercase">1st
-                            Semester</span>
-                    </th>
-                </tr>
-                <tr>
-                    <th class="text-center">1.</th>
-                    <td class="text-center">MT 143</td>
-                    <td>Plane & Spherical Trigonometry</td>
-                    <td class="text-center">3.0</td>
-                    <td class="text-center">1.00</td>
-                    <td class="text-center">P</td>
-                </tr>
-                <tr>
-                    <th class="text-center">2.</th>
-                    <td class="text-center">MT 123</td>
-                    <td>Calculus</td>
-                    <td class="text-center">3.0</td>
-                    <td class="text-center">1.75</td>
-                    <td class="text-center">P</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+        @foreach ($vd['stud_grades'] as $gradeSheetPerYear)
+            {{-- <h1>{{ $gradeSheetPerYear['acad_year_long'] }}</h1> --}}
+            <table class="table table-bordered table-sm">
+                <thead>
+                    <tr class="table-active bg-secondary">
+                        <th scope="col" colspan="6" class="py-3"><span
+                                class="text-uppercase">{{ $gradeSheetPerYear['acad_year_long'] }}</span>
+                        </th>
+                    </tr>
+                    <tr class="table-active">
+                        <th scope="col"
+                            width="5%"class="text-center font-weight-normal small align-middle font-weight-bold">NO.
+                        </th>
+                        <th scope="col" width="15%"
+                            class="font-weight-normal small align-middle font-weight-bold">
+                            SUBJECT CODE</th>
+                        <th scope="col" width="65%"
+                            class="font-weight-normal small align-middle font-weight-bold">
+                            DESCRIPTION</th>
+                        <th scope="col" width="5%"
+                            class="font-weight-normal small align-middle font-weight-bold">
+                            UNITS</th>
+                        <th scope="col" width="5%"
+                            class="font-weight-normal small align-middle font-weight-bold">
+                            FINAL GRADE</th>
+                        <th scope="col" width="5%"
+                            class="font-weight-normal small align-middle font-weight-bold">
+                            REMARKS</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($gradeSheetPerYear['semesters'] as $semester)
+                        <tr>
+                            <th scope="col" colspan="6"class="align-middle text-center"><span
+                                    class="text-uppercase">{{ $semester['term_name'] }}</span>
+                            </th>
+                        </tr>
+                        @foreach ($semester['subjects'] as $key => $subject)
+                            <tr>
+                                <th class="text-center">{{ $key + 1 }}.</th>
+                                <td class="text-center">{{ $subject['enrsub_subj_code'] }}</td>
+                                <td>{{ $subject['enrsub_subj_name'] }}</td>
+                                <td class="text-center">{{ $subject['enrsub_subj_units'] }}</td>
+                                <td class="text-center">{{ $subject['enrsub_finalGrade'] }}</td>
+                                <td class="text-center">{{ $subject['enrsub_grade_status'] }}</td>
+                            </tr>
+                        @endforeach
+                    @endforeach
 
-    <div>
-        <table class="table table-bordered table-sm">
-            <thead>
-                <tr class="table-active bg-secondary">
-                    <th scope="col" colspan="6" class="py-3"><span class="text-uppercase">SY 1999 - 2000</span>
-                    </th>
-                </tr>
-                <tr class="table-active">
-                    <th scope="col"
-                        width="5%"class="text-center font-weight-normal small align-middle font-weight-bold">NO.
-                    </th>
-                    <th scope="col" width="15%" class="font-weight-normal small align-middle font-weight-bold">
-                        SUBJECT CODE</th>
-                    <th scope="col" width="65%" class="font-weight-normal small align-middle font-weight-bold">
-                        DESCRIPTION</th>
-                    <th scope="col" width="5%" class="font-weight-normal small align-middle font-weight-bold">
-                        UNITS</th>
-                    <th scope="col" width="5%" class="font-weight-normal small align-middle font-weight-bold">
-                        FINAL GRADE</th>
-                    <th scope="col" width="5%" class="font-weight-normal small align-middle font-weight-bold">
-                        GRADE STATUS</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="col" colspan="6"class="align-middle text-center"><span class="text-uppercase">2nd
-                            Semester</span>
-                    </th>
-                </tr>
-                <tr>
-                    <th class="text-center">1.</th>
-                    <td class="text-center">MT 123</td>
-                    <td>COLLEGE ALGEBRA</td>
-                    <td class="text-center">3.0</td>
-                    <td class="text-center">2.50</td>
-                    <td class="text-center">P</td>
-                </tr>
-                <tr>
-                    <th scope="col" colspan="6"class="align-middle text-center"><span class="text-uppercase">1st
-                            Semester</span>
-                    </th>
-                </tr>
-                <tr>
-                    <th class="text-center">1.</th>
-                    <td class="text-center">MT 143</td>
-                    <td>Plane & Spherical Trigonometry</td>
-                    <td class="text-center">3.0</td>
-                    <td class="text-center">1.00</td>
-                    <td class="text-center">P</td>
-                </tr>
-                <tr>
-                    <th class="text-center">2.</th>
-                    <td class="text-center">MT 123</td>
-                    <td>Calculus</td>
-                    <td class="text-center">3.0</td>
-                    <td class="text-center">1.75</td>
-                    <td class="text-center">P</td>
-                </tr>
-            </tbody>
-        </table>
+
+
+                </tbody>
+            </table>
+        @endforeach
     </div>
 </body>
 
