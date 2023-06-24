8563504a266d05d64551b7cca48dcdce9e526687
Andrei Lazaro <alazaro.apps@gmail.com>
2023-06-24 10:07:24 +0800

    Fix System Setups.

--- resources/lang/en/panel.php
+++ resources/lang/en/panel.php
@@ -3,4 +3,5 @@
 return [
     'site_title' => config('app.name'),
     'site_title_short' => config('app.name_short'),
+    'setup_description' => 'This is a crucial aspect of SRIMS, as it involves organizing and configuring the software components necessary for the application to function optimally.',
 ];
