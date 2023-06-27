34d860c4eda5fc325b115b882f074a741a96e312
Andrei Lazaro <alazaro.apps@gmail.com>
2023-06-27 09:55:53 +0800

    Update Automated backup

--- config/app.php
+++ config/app.php
@@ -197,7 +197,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Spatie\Backup\BackupServiceProvider::class,
         /*
          * Application Service Providers...
          */
